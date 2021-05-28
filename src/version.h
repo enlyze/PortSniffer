b690de9819c83d4555d2e5420274c7b170cbd077
Colin Finck <c.finck@enlyze.com>
2021-05-28 16:14:28 +0200

    Version 1.2

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 //
 // PortSniffer - Monitor the traffic of arbitrary serial or parallel ports
-// Copyright 2020 Colin Finck, ENLYZE GmbH <c.finck@enlyze.com>
+// Copyright 2020-2021 Colin Finck, ENLYZE GmbH <c.finck@enlyze.com>
 //
 // SPDX-License-Identifier: MIT
 //
@@ -10,7 +10,7 @@
 // We use Semantic Versioning (https://semver.org) without a patch version here.
 // Increase the major version on API-incompatible changes, increase the minor version on API-compatible changes.
 #define PORTSNIFFER_MAJOR_VERSION       1
-#define PORTSNIFFER_MINOR_VERSION       1
+#define PORTSNIFFER_MINOR_VERSION       2
 
 // The following two lines of macro magic turn arbitrary preprocessor constants into strings.
 #define STRINGIFY_INTERNAL(x)           #x
